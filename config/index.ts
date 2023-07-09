2f1d148424e341d6f82670d0ed570db7f4db74a1
黎翔宇 <30423790+kekexililxy@users.noreply.github.com>
2023-07-09 22:30:32 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,6 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = ''
-export const API_KEY = ''
+export const APP_ID = '55d17abc-08b5-441b-999a-e50817637ac4'
+export const API_KEY = '<Web API Key From Dify>'
 export const API_URL = ''
 export const APP_INFO: AppInfo = {
   title: 'Chat APP',
