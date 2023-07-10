abfaadfa34d50005660e45de806f904e05ce3920
黎翔宇 <30423790+kekexililxy@users.noreply.github.com>
2023-07-10 12:49:04 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,7 +3,7 @@ export const APP_ID = '55d17abc-08b5-441b-999a-e50817637ac4'
 export const API_KEY = 'app-teInYbklxbAW4t94kiuoreL3'
 export const API_URL = ''
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
+  title: '建工检测AI',
   description: '',
   copyright: '',
   privacy_policy: '',
